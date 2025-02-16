7aae13790d0f0b97addc6eeb597c45384fb48c16
surendhar19 <34364664+surendhar19@users.noreply.github.com>
2025-02-16 18:10:35 +0530

    Update Controller.java
    added

--- Sprind-Data-JPA-Project/src/main/java/com/datajpa/controller/Controller.java
+++ Sprind-Data-JPA-Project/src/main/java/com/datajpa/controller/Controller.java
@@ -2,7 +2,7 @@ package com.datajpa.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//this is teest
 @RestController
 public class Controller {
     @GetMapping("/welcome")
