0f41340fb1045bebc6dc3484c7a0fce35c4aeb37
surendhar19 <surendhar1953@gmail.com>
2025-02-16 18:27:42 +0530

    adding new project

--- Sprind-Data-JPA-Project/src/main/java/com/datajpa/controller/Controller.java
+++ Sprind-Data-JPA-Project/src/main/java/com/datajpa/controller/Controller.java
@@ -2,7 +2,7 @@ package com.datajpa.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//adding new changes.
 @RestController
 public class Controller {
     @GetMapping("/welcome")
