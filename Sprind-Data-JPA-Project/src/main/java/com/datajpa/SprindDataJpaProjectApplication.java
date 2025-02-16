1395d04829949d01321ba39520706f2d783d8f7f
surendhar19 <surendhar1953@gmail.com>
2025-02-16 17:50:05 +0530

    adding new project

---
+++ Sprind-Data-JPA-Project/src/main/java/com/datajpa/SprindDataJpaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprindDataJpaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprindDataJpaProjectApplication.class, args);
+	}
+
+}
